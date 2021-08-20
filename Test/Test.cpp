2c0a3ddd072382e8504f7037a68e70dcc9ce6aa0
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2021-08-21 00:13:22 +1000

    Adding __m256i support

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,36 +2,32 @@
 //TODO Add Vector.h, does that also need a tt file?
 #include "Vector.cpp"
 
-void branches1()
+void simple128test()
 {
 	constexpr int array[4] = { 4, 7, -2, 9 };
 	constexpr int value = 5, adjustment1 = 10, adjustment2 = 3;
 
 	auto vArray = vector<int, 128>(array[0], array[1], array[2], array[3]);
 
-	vArray += vector<int, 128>::Blend(vArray > value, adjustment2, adjustment1);
+	vArray += vector<int, 128>::Blend(vArray > static_cast<vector<int, 128>>(value), adjustment2, adjustment1);
 
-	std::cout << "branches1 vector:" << std::endl << vArray << std::endl << std::endl;
+	std::cout << "simple 128 bit test:" << std::endl << vArray << std::endl << std::endl;
 }
 
-/*void branches2()
+void simple256test()
 {
 	constexpr int array[8] = { 4, -2, 9, 7, 3, 2, 4, 6 };
 	constexpr int value = 5, adjustment = 3;
 
-	auto vArray = vectorold<int, 256>(array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7]);
-	
+	auto vArray = vector<int, 256>(array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7]);
 
-	const auto vComparision = vArray > value;
-	//const __m256i vResult = _mm256_blendv_epi8(vAdjustment1, vAdjustment2, vComparision);
-	//vArray = _mm256_add_epi32(vArray, vResult);
+	vArray += vector<int, 256>::Blend(vArray > static_cast<vector<int, 256>>(value), adjustment, 0);
 
-	//printf("branches2 scalar:\n%d %d %d %d %d %d %d %d\n", array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7]);
-	//printf("branches2 vector:\n%d %d %d %d %d %d %d %d\n\n", vArray.m256i_i32[0], vArray.m256i_i32[1], vArray.m256i_i32[2], vArray.m256i_i32[3], vArray.m256i_i32[4], vArray.m256i_i32[5], vArray.m256i_i32[6], vArray.m256i_i32[7]);
-}*/
+	std::cout << "simple 256 bit test" << std::endl << vArray << std::endl << std::endl;
+}
 
 int main()
 {
-	branches1();
-	//branches2();
+	simple128test();
+	simple256test();
 }
--- Vectors/Vector.cpp
+++ Vectors/Vector.cpp
@@ -1,26 +1,52 @@
 //TODO Merge string info arrays into a single structure
 #include <intrin.h>
+#include <ostream>
+#include <sstream>
 
 template <uint_fast16_t X>
 _INLINE_VAR constexpr bool is_simd_128 = X == 128;
+
+template <uint_fast16_t X>
+_INLINE_VAR constexpr bool is_simd_256 = X == 256;
 
 template<typename T, uint_fast16_t X>
 struct vector
 {
 private:
 	__m128i vector_128_{};
+	__m256i vector_256_{};
 
 	//TODO Generate automatically?
 	vector(const __m128i vector) : vector_128_(vector)
 	{
 	}
 
+	vector(const __m256i vector) : vector_256_(vector)
+	{
+	}
+
 public:
+	//TODO Check if these can be merged
+	//ToString Functions
 	[[nodiscard]] std::string ToString128i() const
 	{
 		std::stringstream sstr;
 		T values[16 / sizeof(T)];
 		std::memcpy(values, &this->vector_128_, sizeof(values));
+
+		for (T v : values)
+		{
+			sstr << v << " ";
+		}
+
+		return sstr.str();
+	}
+
+	[[nodiscard]] std::string ToString256i() const
+	{
+		std::stringstream sstr;
+		T values[32 / sizeof(T)];
+		std::memcpy(values, &this->vector_256_, sizeof(values));
 
 		for (T v : values)
 		{
@@ -55,9 +81,41 @@ public:
 	}
 
 	// Comparision Operators
+	//TODO Fix this not working allow implicit casts when more than one version exist
 	vector<int32_t, 128> operator>(const vector<int32_t, 128>& vector2) const
 	{
 		return _mm_cmpgt_epi32(this->vector_128_, vector2.vector_128_);
+	}
+	#pragma endregion
+	#pragma region vector<int32_t, 256>
+
+	// Constructors
+	vector(const int32_t value) requires(std::is_same_v<T, int32_t> && is_simd_256<X>) : vector_256_(_mm256_set1_epi32(value))
+	{
+	}
+
+	explicit vector(const int32_t v1, const int32_t v2, const int32_t v3, const int32_t v4, const int32_t v5, const int32_t v6, const int32_t v7, const int32_t v8) requires(std::is_same_v<T, int32_t> && is_simd_256<X>) : vector_256_(_mm256_setr_epi32(v1, v2, v3, v4, v6, v6, v7, v8))
+	{
+	}
+
+	//TODO Figure out how to make the types of the parameters enough to figure out which version to use and not require specifying when accessing vector
+	vector<int32_t, 256> static Blend(const vector<int32_t, 256> comparision, const vector<int32_t, 256> falseValue, const vector<int32_t, 256> trueValue)
+	{
+		return _mm256_blendv_epi8(falseValue.vector_256_, trueValue.vector_256_, comparision.vector_256_);
+	}
+
+	// Assignment Operators
+	vector& operator+=(const vector<int32_t, 256>& rhs)
+	{
+		this->vector_256_ = _mm256_add_epi32(this->vector_256_, rhs.vector_256_);
+		return *this;
+	}
+
+	// Comparision Operators
+	//TODO Fix this not working allow implicit casts when more than one version exist
+	vector<int32_t, 256> operator>(const vector<int32_t, 256>& vector2) const
+	{
+		return _mm256_cmpgt_epi32(this->vector_256_, vector2.vector_256_);
 	}
 	#pragma endregion
 };
@@ -67,4 +125,8 @@ public:
 inline std::ostream& operator<<(std::ostream& stream, const vector<int32_t, 128>& vector)
 {
 	return stream << vector.ToString128i();
+}
+inline std::ostream& operator<<(std::ostream& stream, const vector<int32_t, 256>& vector)
+{
+	return stream << vector.ToString256i();
 }
