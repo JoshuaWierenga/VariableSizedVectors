8a4fe5ee4d06a018d02506f8dd1cd2a4db9bb847
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2022-12-21 17:38:17 +1100

    Third attempt

---
+++ Vectors/RawMemory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace ArbitraryLengthVectors;
+
+// Based on https://github.com/dotnet/runtime/issues/13485#issuecomment-535161422
+internal sealed unsafe class RawMemory : IDisposable
+{
+    private readonly MemoryMappedFile _mapping;
+    private readonly MemoryMappedViewAccessor _accessor;
+
+    internal void* MemoryAddress { get; }
+
+    public RawMemory(int sizeInBytes)
+    {
+        _mapping = MemoryMappedFile.CreateNew(null, sizeInBytes);
+        _accessor = _mapping.CreateViewAccessor();
+        MemoryAddress = (void*)_accessor.SafeMemoryMappedViewHandle.DangerousGetHandle();
+    }
+
+    ~RawMemory()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (!disposing) return;
+
+        _mapping.Dispose();
+        _accessor.Dispose();
+    }
+}
--- VectorsTest/Program.cs
+++ VectorsTest/Program.cs
@@ -1,117 +1,116 @@
 ﻿using System;
-using Vectors;
+using ArbitraryLengthVectors;
 
-namespace VectorsTest
+namespace VectorsTest;
+
+internal static class Program
 {
-    class Program
+    private static void Main()
     {
-        static void Main(string[] args)
-        {
-            TestVectorDouble();
-        }
+        TestVector();
+    }
 
-        static void TestVectorDouble()
-        {
+    private static void TestVector()
+    {
 
-            Console.WriteLine("Byte single value vector");
-            Vector<byte> firstByte = new(6);
-            Vector<byte> secondByte = new(2);
+        Console.WriteLine("Byte single value vector");
+        Vector<byte> firstByte = new(6);
+        Vector<byte> secondByte = new(2);
             
-            Console.WriteLine($"{firstByte} + {secondByte} = {firstByte + secondByte}");
-            Console.WriteLine($"{firstByte} - {secondByte} = {firstByte - secondByte}");
-            Console.WriteLine($"{firstByte} * {secondByte} = {firstByte * secondByte}");
-            Console.WriteLine($"{firstByte} * {2} = {firstByte * 2}");
-            Console.WriteLine($"{6} * {secondByte} = {6 * secondByte}");
-            Console.WriteLine($"{firstByte} / {secondByte} = {firstByte / secondByte}");
-            Console.WriteLine($"-{firstByte} = {-firstByte}");
-            Console.WriteLine($"{firstByte} == {secondByte} = {firstByte == secondByte}");
-            Console.WriteLine($"{firstByte} == {firstByte} = {firstByte == firstByte}");
-            Console.WriteLine($"{firstByte} != {secondByte} = {firstByte != secondByte}");
+        Console.WriteLine($"{firstByte} + {secondByte} = {firstByte + secondByte}");
+        /*Console.WriteLine($"{firstByte} - {secondByte} = {firstByte - secondByte}");
+        Console.WriteLine($"{firstByte} * {secondByte} = {firstByte * secondByte}");
+        Console.WriteLine($"{firstByte} * {2} = {firstByte * 2}");
+        Console.WriteLine($"{6} * {secondByte} = {6 * secondByte}");
+        Console.WriteLine($"{firstByte} / {secondByte} = {firstByte / secondByte}");
+        Console.WriteLine($"-{firstByte} = {-firstByte}");
+        Console.WriteLine($"{firstByte} == {secondByte} = {firstByte == secondByte}");
+        Console.WriteLine($"{firstByte} == {firstByte} = {firstByte == firstByte}");
+        Console.WriteLine($"{firstByte} != {secondByte} = {firstByte != secondByte}");*/
 
-            Console.WriteLine();
+        Console.WriteLine();
 
-            Console.WriteLine("Ulong double value vector");
-            Vector<ulong> firstUlong = new(new ulong[] { 6, 9 });
-            Vector<ulong> secondUlong = new(new ulong[] { 2, 3 });
+        Console.WriteLine("Ulong double value vector");
+        Vector<ulong> firstUlong = new(6, 9);
+        Vector<ulong> secondUlong = new(2, 3);
 
-            Console.WriteLine($"{firstUlong} + {secondUlong} = {firstUlong + secondUlong}");
-            Console.WriteLine($"{firstUlong} - {secondUlong} = {firstUlong - secondUlong}");
-            Console.WriteLine($"{firstUlong} * {secondUlong} = {firstUlong * secondUlong}");
-            Console.WriteLine($"{firstUlong} * {2} = {firstUlong * 2}");
-            Console.WriteLine($"{6} * {secondUlong} = {6 * secondUlong}");
-            Console.WriteLine($"{firstUlong} / {secondUlong} = {firstUlong / secondUlong}");
-            Console.WriteLine($"-{firstUlong} = {-firstUlong}");
-            Console.WriteLine($"{firstUlong} == {secondUlong} = {firstUlong == secondUlong}");
-            Console.WriteLine($"{firstUlong} == {firstUlong} = {firstUlong == firstUlong}");
-            Console.WriteLine($"{firstUlong} != {secondUlong} = {firstUlong != secondUlong}");
+        Console.WriteLine($"{firstUlong} + {secondUlong} = {firstUlong + secondUlong}");
+        /*Console.WriteLine($"{firstUlong} - {secondUlong} = {firstUlong - secondUlong}");
+        Console.WriteLine($"{firstUlong} * {secondUlong} = {firstUlong * secondUlong}");
+        Console.WriteLine($"{firstUlong} * {2} = {firstUlong * 2}");
+        Console.WriteLine($"{6} * {secondUlong} = {6 * secondUlong}");
+        Console.WriteLine($"{firstUlong} / {secondUlong} = {firstUlong / secondUlong}");
+        Console.WriteLine($"-{firstUlong} = {-firstUlong}");
+        Console.WriteLine($"{firstUlong} == {secondUlong} = {firstUlong == secondUlong}");
+        Console.WriteLine($"{firstUlong} == {firstUlong} = {firstUlong == firstUlong}");
+        Console.WriteLine($"{firstUlong} != {secondUlong} = {firstUlong != secondUlong}");*/
 
-            Console.WriteLine();
+        Console.WriteLine();
 
-            Console.WriteLine("Int triple value vector");
-            Vector<int> firstInt = new(new[] { 6, 9, -20 });
-            Vector<int> secondInt = new(new[] { 2, 3, 10 });
+        Console.WriteLine("Int triple value vector");
+        Vector<int> firstInt = new(6, 9, -20);
+        Vector<int> secondInt = new(2, 3, 10);
 
-            Console.WriteLine($"{firstInt} + {secondInt} = {firstInt + secondInt}");
-            Console.WriteLine($"{firstInt} - {secondInt} = {firstInt - secondInt}");
-            Console.WriteLine($"{firstInt} * {secondInt} = {firstInt * secondInt}");
-            Console.WriteLine($"{firstInt} * {2} = {firstInt * 2}");
-            Console.WriteLine($"{6} * {secondInt} = {6 * secondInt}");
-            Console.WriteLine($"{firstInt} / {secondInt} = {firstInt / secondInt}");
-            Console.WriteLine($"-{firstInt} = {-firstInt}");
-            Console.WriteLine($"{firstInt} == {secondInt} = {firstInt == secondInt}");
-            Console.WriteLine($"{firstInt} == {firstInt} = {firstInt == firstInt}");
-            Console.WriteLine($"{firstInt} != {secondInt} = {firstInt != secondInt}");
+        Console.WriteLine($"{firstInt} + {secondInt} = {firstInt + secondInt}");
+        /*Console.WriteLine($"{firstInt} - {secondInt} = {firstInt - secondInt}");
+        Console.WriteLine($"{firstInt} * {secondInt} = {firstInt * secondInt}");
+        Console.WriteLine($"{firstInt} * {2} = {firstInt * 2}");
+        Console.WriteLine($"{6} * {secondInt} = {6 * secondInt}");
+        Console.WriteLine($"{firstInt} / {secondInt} = {firstInt / secondInt}");
+        Console.WriteLine($"-{firstInt} = {-firstInt}");
+        Console.WriteLine($"{firstInt} == {secondInt} = {firstInt == secondInt}");
+        Console.WriteLine($"{firstInt} == {firstInt} = {firstInt == firstInt}");
+        Console.WriteLine($"{firstInt} != {secondInt} = {firstInt != secondInt}");*/
 
-            Console.WriteLine();
+        Console.WriteLine();
 
-            Console.WriteLine("UShort quad value vector");
-            Vector<ushort> firstUShort = new(new ushort[] { 6, 9, 20, 100 });
-            Vector<ushort> secondUShort = new(new ushort[] { 2, 3, 10, 50 });
+        Console.WriteLine("UShort quad value vector");
+        Vector<ushort> firstUShort = new(6, 9, 20, 100);
+        Vector<ushort> secondUShort = new(2, 3, 10, 50);
 
-            Console.WriteLine($"{firstUShort} + {secondUShort} = {firstUShort + secondUShort}");
-            Console.WriteLine($"{firstUShort} - {secondUShort} = {firstUShort - secondUShort}");
-            Console.WriteLine($"{firstUShort} * {secondUShort} = {firstUShort * secondUShort}");
-            Console.WriteLine($"{firstUShort} * {2} = {firstUShort * 2}");
-            Console.WriteLine($"{6} * {secondUShort} = {6 * secondUShort}");
-            Console.WriteLine($"{firstUShort} / {secondUShort} = {firstUShort / secondUShort}");
-            Console.WriteLine($"-{firstUShort} = {-firstUShort}");
-            Console.WriteLine($"{firstUShort} == {secondUShort} = {firstUShort == secondUShort}");
-            Console.WriteLine($"{firstUShort} == {firstUShort} = {firstUShort == firstUShort}");
-            Console.WriteLine($"{firstUShort} != {secondUShort} = {firstUShort != secondUShort}");
+        Console.WriteLine($"{firstUShort} + {secondUShort} = {firstUShort + secondUShort}");
+        /*Console.WriteLine($"{firstUShort} - {secondUShort} = {firstUShort - secondUShort}");
+        Console.WriteLine($"{firstUShort} * {secondUShort} = {firstUShort * secondUShort}");
+        Console.WriteLine($"{firstUShort} * {2} = {firstUShort * 2}");
+        Console.WriteLine($"{6} * {secondUShort} = {6 * secondUShort}");
+        Console.WriteLine($"{firstUShort} / {secondUShort} = {firstUShort / secondUShort}");
+        Console.WriteLine($"-{firstUShort} = {-firstUShort}");
+        Console.WriteLine($"{firstUShort} == {secondUShort} = {firstUShort == secondUShort}");
+        Console.WriteLine($"{firstUShort} == {firstUShort} = {firstUShort == firstUShort}");
+        Console.WriteLine($"{firstUShort} != {secondUShort} = {firstUShort != secondUShort}");*/
 
-            Console.WriteLine();
+        Console.WriteLine();
 
-            Console.WriteLine("Double 7 value vector");
-            Vector<double> firstDouble = new(new double[] { 6, 9, 20, 100, 36, 88, 11 });
-            Vector<double> secondDouble = new(new double[] { 2, 3, 10, 50, 8, 45, 11 });
+        Console.WriteLine("Double 7 value vector");
+        Vector<double> firstDouble = new(6, 9, 20, 100, 36, 88, 11);
+        Vector<double> secondDouble = new(2, 3, 10, 50, 8, 45, 11);
 
-            Console.WriteLine($"{firstDouble} + {secondDouble} = {firstDouble + secondDouble}");
-            Console.WriteLine($"{firstDouble} - {secondDouble} = {firstDouble - secondDouble}");
-            Console.WriteLine($"{firstDouble} * {secondDouble} = {firstDouble * secondDouble}");
-            Console.WriteLine($"{firstDouble} * {2} = {firstDouble * 2}");
-            Console.WriteLine($"{6} * {secondDouble} = {6 * secondDouble}");
-            Console.WriteLine($"{firstDouble} / {secondDouble} = {firstDouble / secondDouble}");
-            Console.WriteLine($"-{firstDouble} = {-firstDouble}");
-            Console.WriteLine($"{firstDouble} == {secondDouble} = {firstDouble == secondDouble}");
-            Console.WriteLine($"{firstDouble} == {firstDouble} = {firstDouble == firstDouble}");
-            Console.WriteLine($"{firstDouble} != {secondDouble} = {firstDouble != secondDouble}");
+        Console.WriteLine($"{firstDouble} + {secondDouble} = {firstDouble + secondDouble}");
+        /*Console.WriteLine($"{firstDouble} - {secondDouble} = {firstDouble - secondDouble}");
+        Console.WriteLine($"{firstDouble} * {secondDouble} = {firstDouble * secondDouble}");
+        Console.WriteLine($"{firstDouble} * {2} = {firstDouble * 2}");
+        Console.WriteLine($"{6} * {secondDouble} = {6 * secondDouble}");
+        Console.WriteLine($"{firstDouble} / {secondDouble} = {firstDouble / secondDouble}");
+        Console.WriteLine($"-{firstDouble} = {-firstDouble}");
+        Console.WriteLine($"{firstDouble} == {secondDouble} = {firstDouble == secondDouble}");
+        Console.WriteLine($"{firstDouble} == {firstDouble} = {firstDouble == firstDouble}");
+        Console.WriteLine($"{firstDouble} != {secondDouble} = {firstDouble != secondDouble}");*/
 
-            Console.WriteLine();
+        Console.WriteLine();
 
-            Console.WriteLine("Byte 20 value vector");
-            firstByte = new Vector<byte>(new byte[] { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60 });
-            secondByte = new Vector<byte>(new byte[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 });
+        Console.WriteLine("Byte 20 value vector");
+        firstByte = new Vector<byte>(3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60);
+        secondByte = new Vector<byte>(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40);
 
-            Console.WriteLine($"{firstByte} + {secondByte} = {firstByte + secondByte}");
-            Console.WriteLine($"{firstByte} - {secondByte} = {firstByte - secondByte}");
-            Console.WriteLine($"{firstByte} * {secondByte} = {firstByte * secondByte}");
-            Console.WriteLine($"{firstByte} * {2} = {firstByte * 2}");
-            Console.WriteLine($"{6} * {secondByte} = {6 * secondByte}");
-            Console.WriteLine($"{firstByte} / {secondByte} = {firstByte / secondByte}");
-            Console.WriteLine($"-{firstByte} = {-firstByte}");
-            Console.WriteLine($"{firstByte} == {secondByte} = {firstByte == secondByte}");
-            Console.WriteLine($"{firstByte} == {firstByte} = {firstByte == firstByte}");
-            Console.WriteLine($"{firstByte} != {secondByte} = {firstByte != secondByte}");
-        }
+        Console.WriteLine($"{firstByte} + {secondByte} = {firstByte + secondByte}");
+        /*Console.WriteLine($"{firstByte} - {secondByte} = {firstByte - secondByte}");
+        Console.WriteLine($"{firstByte} * {secondByte} = {firstByte * secondByte}");
+        Console.WriteLine($"{firstByte} * {2} = {firstByte * 2}");
+        Console.WriteLine($"{6} * {secondByte} = {6 * secondByte}");
+        Console.WriteLine($"{firstByte} / {secondByte} = {firstByte / secondByte}");
+        Console.WriteLine($"-{firstByte} = {-firstByte}");
+        Console.WriteLine($"{firstByte} == {secondByte} = {firstByte == secondByte}");
+        Console.WriteLine($"{firstByte} == {firstByte} = {firstByte == firstByte}");
+        Console.WriteLine($"{firstByte} != {secondByte} = {firstByte != secondByte}");*/
     }
 }
